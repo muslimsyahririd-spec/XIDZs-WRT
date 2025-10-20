8001ee8b1662f682347d1dac93f0c6984faf6d79
de quenx <fidzxidz@gmail.com>
2025-10-21 02:53:27 +0700

    Update free.sh

--- files/sbin/free.sh
+++ files/sbin/free.sh
@@ -1,1 +1,7 @@
-sync && echo 3 > /proc/sys/vm/drop_caches && rm -rf /tmp/luci*
+#!/bin/sh
+sync && echo 3 > /proc/sys/vm/drop_caches
+rm -rf /tmp/luci* /tmp/*.tmp
+> /var/log/messages
+> /var/log/syslog  
+> /var/log/kern.log
+/etc/init.d/uhttpd restart
