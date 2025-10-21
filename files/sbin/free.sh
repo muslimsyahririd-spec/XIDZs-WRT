9e4af540e3337d5c639e115c23910b447710ca8c
de quenx <fidzxidz@gmail.com>
2025-10-21 13:37:32 +0700

    Update free.sh

--- files/sbin/free.sh
+++ files/sbin/free.sh
@@ -1,4 +1,1 @@
-#!/bin/sh
-sync && echo 3 > /proc/sys/vm/drop_caches
-rm -rf /tmp/luci* /tmp/*.tmp
-find /var/log -type f -exec sh -c '> {}' \;
+sync && echo 3 > /proc/sys/vm/drop_caches && rm -rf /tmp/luci*
